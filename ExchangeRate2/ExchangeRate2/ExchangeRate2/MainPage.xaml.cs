883fd38f33bc8fafc3230f913bb7115d62d95e9a
Artem314159 <recruter@i.ua>
2019-04-25 19:00:39 +0300

    Approach is changed

---
+++ ExchangeRate2/ExchangeRate2/ExchangeRate2/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ExchangeRate2
+{
+    public partial class MainPage : ContentPage
+    {
+        public enum Currency
+        {
+            Euro,
+            Dollar,
+            Ruble
+        }
+        public Dictionary<Currency, string> CurrencyText = new Dictionary<Currency, string>
+        {
+            [Currency.Euro] = "€",
+            [Currency.Dollar] = "$",
+            [Currency.Ruble] = "₽",
+        };
+
+        public MainPage()
+        {
+            InitializeComponent();
+            var grid = new Grid() { Margin = 10 };
+            int collInd = 0;
+            foreach (Currency currency in Enum.GetValues(typeof(Currency)))
+            {
+                grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
+                var newBtn = new Button() { Text = CurrencyText[currency] };
+                newBtn.Clicked += delegate (object sender, EventArgs e) { Button_Click(sender, e, currency); };
+                grid.Children.Add(newBtn, collInd, 0);
+                collInd++;
+            }
+            MainLayout.Children.Add(grid);
+        }
+
+        private void Button_Click(object sender, EventArgs e, Currency currency)
+        {
+            DisplayAlert("Title", currency.ToString(), "Ok");
+        }
+    }
+}
